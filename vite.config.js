88807ff83158b506d7a83ea5fc1318e73a9ef67b
염윤철 <yc.yeom@okestro.com>
2025-08-09 07:30:41 +0900

    [feature] Add production build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/seven-knights-reverse-deck/', // 본인 저장소명으로 꼭 수정!
   plugins: [react()],
   server: {
     port: 5173,
